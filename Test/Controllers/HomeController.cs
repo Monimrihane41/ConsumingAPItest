b43d2136648cda6230131fb35dcb9a12b1d9f11e
Abdelmounim Rihane <abdelmounaimrihan@gmail.com>
2023-12-06 02:52:50 +0100

    Adding Api Link

--- Test/Controllers/HomeController.cs
+++ Test/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Data;
 using System.Diagnostics;
+using System.Net.Http.Headers;
 using Test.Models;
 using static System.Net.WebRequestMethods;
 
@@ -8,17 +11,46 @@ namespace Test.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        string baseUrl = "https://api.rawg.io/api/games?key=6126a65e6cb54c538114b08e9aaf3f22";
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
-        public async Task <IActionResult> Index()
+        string baseUrl = "https://api.rawg.io/api/games";
+        string apiKey = "6126a65e6cb54c538114b08e9aaf3f22";
+
+        public async Task<IActionResult> Index()
         {
-            //calling The Web API and populate the data in the view
-            return View();
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    httpClient.BaseAddress = new Uri(baseUrl);
+                    httpClient.DefaultRequestHeaders.Clear();
+                    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    // Append the API key as a query parameter
+                    HttpResponseMessage response = await httpClient.GetAsync($"?key={apiKey}");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string result = await response.Content.ReadAsStringAsync();
+                        var gameList = JsonConvert.DeserializeObject<DataTable>(result);
+                        return View(gameList);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Failed to fetch data. Status Code: {response.StatusCode}, Reason: {response.ReasonPhrase}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+
+            return View("Error");
         }
 
         public IActionResult Privacy()
